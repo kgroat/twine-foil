05818f0e27a32a48ec371115d0e9a28c5e3ce6bf
Kevin Groat <kgroat09@gmail.com>
2016-06-19 10:23:26 -0400

    moved id markup for renderLinks

--- format/helpers/renderLinks.js
+++ format/helpers/renderLinks.js
@@ -8,16 +8,17 @@ function renderLinks(input, replacer){
 function renderSingleLink(link, contents){
   var passage, text, id;
   var match;
-  if(match = /^(.*?)(?:->|\|)(.*?)(?:#(\w+))?$/.exec(contents)){
+  if(match = /^(.*?)(?:#(\w+))?(?:->|\|)(.*?)$/.exec(contents)){
     text = match[1];
-    passage = match[2];
-    id = match[3] || '';
+    passage = match[3];
+    id = match[2] || '';
   } else if(match = /^(.*?)<-(.*?)(?:#(\w+))?$/.exec(contents)){
     text = match[2];
     passage = match[1];
     id = match[3] || '';
   } else {
     passage = text = contents;
+    id = '';
   }
   return '<a id="'+id+'" href="javascript:definer(\'story\').show(\''+passage+'\')" data-passage="'+passage+'">'+text+'</a>';
 }
